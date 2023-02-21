0bae40c0c783b819d57dbe3e5768ff58e4a94502
Pleng Nakdee <plengwn@gmail.com>
2023-02-21 15:34:13 +0700

    bucj=ket stack

--- lib/bucket-stack.ts
+++ lib/bucket-stack.ts
@@ -1,11 +1,19 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class BucketStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'TestBucket', {
+      bucketName: 'test-bucket',
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new CfnOutput(this, 'BucketRef', {
+      value: bucket.bucketName,
+    });
 
   }
 }
